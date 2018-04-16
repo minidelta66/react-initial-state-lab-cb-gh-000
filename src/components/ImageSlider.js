9e5f436a9eee79acb5622b1bbfd7accc28c10c1a
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-16 18:24:41 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,2 +1,20 @@
 // Bomb Component Code Goes Here
 import React from 'react';
+
+class ImageSlider extends React.Component {
+
+  state = {
+    currentSlideIndex: 0;
+  }
+
+  render() {
+    return (
+      "I am on slide ${this.state.currentSlideIndex}"
+
+
+      
+    )
+  }
+}
+
+export default ImageSlider
